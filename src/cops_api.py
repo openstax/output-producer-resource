78f3e7336096a5871cf7397f2d8bbbfb0587302e
Brenda Nguyen <bn16@rice.edu>
2019-12-17 11:56:26 -0600

    log resource response

--- src/cops_api.py
+++ src/cops_api.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+from src.utils import msg
+
 
 def build_url(api_root, *args):
     parts = [api_root]
@@ -12,16 +14,19 @@ def build_url(api_root, *args):
 def get_jobs(api_root):
     url = build_url(api_root, "jobs")
     response = requests.get(url)
+    msg(response.text)
     return response.json()
 
 
 def get_job(api_root, job_id):
     url = build_url(api_root, "jobs", job_id)
     response = requests.get(url)
+    msg(response.text)
     return response.json()
 
 
 def update_job(api_root, job_id, data):
     url = build_url(api_root, "jobs", job_id)
     response = requests.put(url, data=json.dumps(data))
+    msg(response.text)
     return response.json()

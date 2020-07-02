40be448f21cfb2336c5895f0a570a74759382cb4
tomjw64 <tom.jw64@gmail.com>
2020-07-02 09:49:48 -0500

    Upgrade pytest for travis

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'dev': [
-            'pytest',
+            'pytest>=4.6',
             'pytest-vcr',
             'pytest-cov',
             'flake8'

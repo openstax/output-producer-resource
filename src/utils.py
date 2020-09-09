3113d2c0123d10eaef86a9799eff6d28afd64a6e
Thomas Williamson <tomjw64@users.noreply.github.com>
2020-09-09 09:58:32 -0500

    Add logs to check containers in /var/log/check (#13)

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,11 @@ def msg(msg, *args, **kwargs):
     if args or kwargs:
         msg = msg.format(*args, **kwargs)
     print(msg, file=sys.stderr)
+    try:
+        with open('/var/log/check', 'a') as f:
+            f.write("msg:" + msg + "\n")
+    except PermissionError:
+        pass
 
 
 def write_file(filepath, data):
